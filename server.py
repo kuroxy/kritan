ce6115970372094ead29da9dcafad173028a6e66
Okke <okkeroelofsen@gmailcom>
2021-06-02 16:20:18 +0200

    server?

--- server.py
+++ server.py
@@ -13,71 +13,87 @@ from worldgen import world
 
 
 
-print_lock = threading.Lock() 
 
 
-HEADERSIZE = 10
 
 
   
 class server():
+    HEADERSIZE = 10
+
     def __init__(self,worldseed):
         self.mainworld = world(worldseed)
 
         self.host = ""
         self.port = 25565
 
+        self.print_lock = threading.Lock() 
+
+        self.conections = {}
+        self.sendlist = []
 
     def send(self, c, addr, data):
-        message = f"{len(data):<{HEADERSIZE}}"
+        message = f"{len(data):<{server.HEADERSIZE}}"
         message = message.encode("utf-8") + data
 
         
         print(f"sending to {addr} : {data}")
         c.send(message) 
-        
     
 
 
+    def mainloop(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+        s.bind((self.host, self.port)) 
+        print("socket binded to port", self.port) 
+    
+        # put the socket into listening mode 
+        s.listen()
+        print("socket is listening") 
+    
+        # a forever loop until client wants to exit 
+        while True: 
+    
+            # establish connection with client 
+            c, addr = s.accept() 
+            self.conections[addr] = c
 
-def receiving(c,addr):
-    global colormap
-    while True: 
-        
-        # data received from client 
-        data = c.recv(2048) 
-        if not data: 
-            print('Lost conenction') 
-            print_lock.release() 
-            break
-        try:
-            data = str(data.decode('utf-8'))
-            print(f" {addr} : {data} : received")
-            data = data.split("|")
-            colormap[int(data[1])][int(data[0])] = (int(data[2]), int(data[3]),int(data[4]))
-        except:
-            print(print(f" {addr} : {data} : received"))
-
-
-def sending(c,addr):
-    global HEADERSIZE
-    global colormap
-    latesthash = ""
-    while True: 
-        if latesthash != colormaphash(colormap):
-            data = pickle.dumps(colormap)
-            message = f"{len(data):<{HEADERSIZE}}"
-            message = message.encode("utf-8") + data
-
+            # lock acquired by client 
             
-            print(f" {addr} : colormap : send")
-            c.send(message) 
-            latesthash = colormaphash(colormap)
+            print('Connected to :', addr[0], ':', addr[1]) 
+    
+            # Start a new thread and return its identifier 
+            start_new_thread(self.receiving, (c,addr,)) 
 
-            file = open('map.byte', 'wb')
-            pickle.dump(colormap, file)
-            file.close()
+    def receiving(self,c,addr):
+        while True: 
+            
+            # data received from client 
+            data = c.recv(2048) 
+            if not data: 
+                print(f"{addr} lost connection") 
+                self.print_lock.release() 
+                del self.conections[addr]
+                break
+            try:
+                data = str(data.decode('utf-8'))
+                print(f" {addr} : {data} : received")
+                self.processcommand(addr,c,data)
+            except:
+                print(print(f" {addr} : {data} : received"))
 
+
+    def processcommand(self,addr,c,data):
+        pass
+
+    
+    def sending(self):
+        while True:
+            if len(self.sendlist) > 0:
+                message = self.sendlist.pop(0)
+                for addr in self.conections:
+                    c = self.conections[addr]
+                    self.send(c,addr,message)
 
 
 
@@ -106,7 +122,6 @@ def Main():
         # Start a new thread and return its identifier 
         start_new_thread(receiving, (c,addr,)) 
         start_new_thread(sending, (c,addr,)) 
-    s.close() 
   
   
 if __name__ == '__main__': 
