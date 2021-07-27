60747eea9bf4dbd4c02381fb2c24ceebcb926cd9
Okke <kuroxy.mail@gmail.com>
2021-07-28 01:21:20 +0200

    wored on the station class, blockclass

--- libs/blocktype.py
+++ libs/blocktype.py
@@ -1,6 +1,9 @@
 class block:
-    def __init__(self,type="unknown", *args):
+    def __init__(self,type="unknown", **metadata):
         self.type = type
 
-
+        
         self.data = {}
+
+        for key in metadata:
+            self.data[key] = metadata[key]
--- libs/playerclasses.py
+++ libs/playerclasses.py
@@ -1,14 +1,20 @@
 from vectors import Vector2D
+from blocktype import block
 
+class station(object):   
+    def __init__(self,playerid,position):
+        self.playerid = playerid
+        self.position = position    # Vec2D absolute position
 
-class station():    #TODO: 
-    pass
+    def getblock(self):
+        # returns station as blockobject
+        return block("station",)
 
 
 
 class krit(object):
     def __init__(self,position):
-        self.position = position
+        self.position = position    # Vec2D absolute position
         self.direction = Vector2D(0,-1) # POINTING NORTH
 
         self.inventory = [None for _ in range(16)] # krits inventory needs working #TODO
